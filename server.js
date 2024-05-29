0545530a4dc91dda07f50378ef519535cb05129d
ParinazYousefi <parinazyousefi1@gmail.com>
2024-05-28 23:27:03 -0400

    change for deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://main--mood-sync.netlify.app/', credentials: true }));
 
 
 app.use(session({ 
