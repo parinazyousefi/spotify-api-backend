a70991b2aec230b4161195f4f106a47b69334c15
ParinazYousefi <parinazyousefi1@gmail.com>
2024-05-29 14:05:27 -0400

    change for deployment to plylist routes

--- routes/playlistRoutes.js
+++ routes/playlistRoutes.js
@@ -1,11 +1,20 @@
-// routes/playlistRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getRecentlyPlayed, getTopArtists, getTopSongs, getRecommendations, getUser} = require('../controllers/playlistController');
+const { getRecentlyPlayed, getTopArtists, getTopSongs, getRecommendations, getUser } = require('../controllers/playlistController');
 
-router.get('/recently-played', getRecentlyPlayed);
-router.get('/top-artists',getTopArtists);
-router.get('/top-songs',getTopSongs);
-router.get('/recommendations',getRecommendations);
-router.get('/profile',getUser);
+// Middleware function to set CORS headers
+const setCorsHeaders = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://mood-sync.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+};
+
+router.get('/recently-played', setCorsHeaders, getRecentlyPlayed);
+router.get('/top-artists', setCorsHeaders, getTopArtists);
+router.get('/top-songs', setCorsHeaders, getTopSongs);
+router.get('/recommendations', setCorsHeaders, getRecommendations);
+router.get('/profile', setCorsHeaders, getUser);
+
 module.exports = router;
