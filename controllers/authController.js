eafdceb1a4093249445b7814284010c6156b863d
ParinazYousefi <parinazyousefi1@gmail.com>
2024-05-29 12:24:57 -0400

    change for deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,17 +2,28 @@ const passport = require('passport');
 
 const authController = {
   spotifyAuth: passport.authenticate('spotify', {
-    scope: ['user-read-email', 'user-read-recently-played', 'playlist-modify-public', 'user-read-recently-played',' user-top-read',' user-follow-read',' user-follow-modify',' playlist-read-private ','playlist-read-collaborative'],
+    scope: [
+      'user-read-email',
+      'user-read-recently-played',
+      'playlist-modify-public',
+      'user-top-read',
+      'user-follow-read',
+      'user-follow-modify',
+      'playlist-read-private',
+      'playlist-read-collaborative'
+    ],
     showDialog: true,
   }),
 
   spotifyCallback: (req, res) => {
     passport.authenticate('spotify', { failureRedirect: '/' }, (err, user) => {
       if (err || !user) {
+        console.error('Authentication error:', err || 'No user');
         return res.redirect('/');
       }
       req.logIn(user, (err) => {
         if (err) {
+          console.error('Login error:', err);
           return res.redirect('/');
         }
         // Successful authentication, redirect to frontend dashboard
@@ -26,7 +37,6 @@ const authController = {
       res.redirect('/');
     });
   },
-}
-
+};
 
 module.exports = authController;
